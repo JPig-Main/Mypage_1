d46597806fb10feed24eb4f0676ed1bd260c26ce
Jumpei <jumpei141107@gmail.com>
2021-01-01 16:07:09 +0900

    Edit

--- Contents/Typing/main.js
+++ Contents/Typing/main.js
@@ -1,304 +1,300 @@
-'use strict';
+"use strict";
 
 {
-    const words_kind = [
-        // 'red',
-        // 'blue',
-        // 'green',
-        // 'yellow',
-        // 'black',
-        // 'white',
-        // 'orange',
-        // 'purple',
-        // 'brown',
-        // 'pink',
-        // 'lemon',
-        // 'peach',
-        // 'apple',
-        // 'banana',
-        // 'melon',
-        // 'grape',
-        // 'river',
-        // 'sea',
-        // 'hill',
-        // 'lake',
-        // 'forest',
-        // 'mountain',
-        // 'city',
-        // 'town',
-        // 'village',
-        // 'desk',
-        // 'chair',
-        // 'bed',
-        // 'dining',
-        // 'door',
-        // 'floor',
-        // 'kitchen',
-        // 'knife',
-        // 'shelf',
-        // 'english',
-        // 'japanese',
-        // 'math',
-        // 'science',
-        // 'history',
-        // 'sunday',
-        // 'monday',
-        // 'tuesday',
-        // 'wednesday',
-        // 'thursday',
-        // 'friday',
-        // 'saturday',
-        // 'january',
-        // 'february',
-        // 'march',
-        // 'april',
-        // 'may',
-        // 'june',
-        // 'july',
-        // 'august',
-        // 'september',
-        // 'october',
-        // 'november',
-        // 'december',
-        // 'spring',
-        // 'summer',
-        // 'fall',
-        // 'winter',
-        'use strict',
-        'const',
-        'let',
-        'console.log',
-        'function',
-        'for',
-        'switch',
-        'while',
-        'break',
-        'case',
-        'continue',
-        'default',
-        'src',
-        'parseInt',
-        'length',
-        'unshift',
-        'shift',
-        'push',
-        'pop',
-        'splice',
-        'forEach',
-        'every',
-        'some',
-        'filter',
-        'includes',
-        'indexOf',
-        'slice',
-        'concat',
-        'find',
-        'flat',
-        'map',
-        'flatMap',
-        'set',
-        'delete',
-        'Object.keys',
-        'split',
-        'join',
-        'substring',
-        'Date',
-        'Date.now',
-        'alert',
-        'confirm',
-        'prompt',
-        'setTimeout',
-        'clearTimeout',
-        'setInterval',
-        'clearInterval',
-        'try',
-        'catch',
-        'throw',
-        'finally',
-        'message',
-        'typeof',
-        'instanceof',
-        'class',
-        'extends',
-        'constructor',
-        'super',
-        'async',
-        'this',
-        'await',
-        'type',
+  const words_kind = [
+    "red",
+    "blue",
+    "green",
+    "yellow",
+    "black",
+    "white",
+    "orange",
+    "purple",
+    "brown",
+    "pink",
+    "lemon",
+    "peach",
+    "apple",
+    "banana",
+    "melon",
+    "grape",
+    "river",
+    "sea",
+    "hill",
+    "lake",
+    "forest",
+    "mountain",
+    "city",
+    "town",
+    "village",
+    "desk",
+    "chair",
+    "bed",
+    "dining",
+    "door",
+    "floor",
+    "kitchen",
+    "knife",
+    "shelf",
+    "english",
+    "japanese",
+    "math",
+    "science",
+    "history",
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "january",
+    "february",
+    "march",
+    "april",
+    "may",
+    "june",
+    "july",
+    "august",
+    "september",
+    "october",
+    "november",
+    "december",
+    "spring",
+    "summer",
+    "fall",
+    "winter",
+    // 'use strict',
+    // 'const',
+    // 'let',
+    // 'console.log',
+    // 'function',
+    // 'for',
+    // 'switch',
+    // 'while',
+    // 'break',
+    // 'case',
+    // 'continue',
+    // 'default',
+    // 'src',
+    // 'parseInt',
+    // 'length',
+    // 'unshift',
+    // 'shift',
+    // 'push',
+    // 'pop',
+    // 'splice',
+    // 'forEach',
+    // 'every',
+    // 'some',
+    // 'filter',
+    // 'includes',
+    // 'indexOf',
+    // 'slice',
+    // 'concat',
+    // 'find',
+    // 'flat',
+    // 'map',
+    // 'flatMap',
+    // 'set',
+    // 'delete',
+    // 'Object.keys',
+    // 'split',
+    // 'join',
+    // 'substring',
+    // 'Date',
+    // 'Date.now',
+    // 'alert',
+    // 'confirm',
+    // 'prompt',
+    // 'setTimeout',
+    // 'clearTimeout',
+    // 'setInterval',
+    // 'clearInterval',
+    // 'try',
+    // 'catch',
+    // 'throw',
+    // 'finally',
+    // 'message',
+    // 'typeof',
+    // 'instanceof',
+    // 'class',
+    // 'extends',
+    // 'constructor',
+    // 'super',
+    // 'async',
+    // 'this',
+    // 'await',
+    // 'type',
+  ];
+  const words = [];
 
-    ];
-    const words = [];
+  let word;
+  let location = 0;
 
-    let word;
-    let location = 0;
+  const STATUS_TEMPLATE = {
+    INIT: "init",
+    READY: "ready",
+    START: "start",
+    FINISH: "finish",
+  };
+  let status = STATUS_TEMPLATE.INIT;
 
-    const STATUS_TEMPLATE = {
-        INIT: 'init',
-        READY: 'ready',
-        START: 'start',
-        FINISH: 'finish',
-    }
-    let status = STATUS_TEMPLATE.INIT;
+  let start_time;
+  let mistake_count;
+  const word_typing_num = 20;
+  let rest_word_num;
 
-    let start_time;
-    let mistake_count;
-    const word_typing_num = 20;
-    let rest_word_num;
+  /* 要素の取得 */
+  const target = document.getElementById("target");
+  const elapsed_time_render = document.getElementById("elapsed-time-render");
+  const rest_word_num_render = document.getElementById("rest-word-num-render");
+  const mistake_render = document.getElementById("mistake-render");
+  const result = document.getElementById("result");
 
-    /* 要素の取得 */
-    const target = document.getElementById('target');
-    const elapsed_time_render = document.getElementById('elapsed-time-render');
-    const rest_word_num_render = document.getElementById('rest-word-num-render');
-    const mistake_render = document.getElementById('mistake-render');
-    const result = document.getElementById('result');
+  /* 単語をセット */
+  function setWord() {
+    word = words[Math.floor(Math.random() * words.length)];
+    target.textContent = word;
+    location = 0;
+  }
 
-    /* 単語をセット */
-    function setWord(){
-        word = words[Math.floor(Math.random() * words.length)];
-        target.textContent = word;
-        location = 0;
-    }
+  /* 1ゲームの準備 */
+  function oneGamePrepare() {
+    /* 単語を準備する */
+    words.splice(0, words.length, ...words_kind);
+    setWord();
 
-    /* 1ゲームの準備 */
-    function oneGamePrepare()
-    {
-        /* 単語を準備する */
-        words.splice(0, words.length, ...words_kind);
-        setWord();
+    /* ミスタッチをクリアする */
+    mistake_count = 0;
+    rest_word_num = word_typing_num;
 
-        /* ミスタッチをクリアする */
-        mistake_count = 0;
-        rest_word_num = word_typing_num;
+    /* 下部分の表示を準備する */
+    renderElapsedTime(0);
+    rest_word_num_render.textContent = `残り単語数: ${word_typing_num}`;
+    mistake_render.textContent = `ミスタッチ: ${mistake_count}`;
+    result.textContent = "単語の頭文字から打っていってください。";
 
-        /* 下部分の表示を準備する */
-        renderElapsedTime(0);
-        rest_word_num_render.textContent = `残り単語数: ${word_typing_num}`;
-        mistake_render.textContent = `ミスタッチ: ${mistake_count}`;
-        result.textContent = '単語の頭文字から打っていってください。';
+    /* ステータスを準備完了にする */
+    status = STATUS_TEMPLATE.READY;
+  }
 
-        /* ステータスを準備完了にする */
-        status = STATUS_TEMPLATE.READY;
-    }
-
-    /* 経過時間の表示
+  /* 経過時間の表示
         ・引数: <int manual_time> 手動の場合の経過時間。自動の場合はnull(初期値)
     */
-    function renderElapsedTime (manual_time = null)
-    {
-        let minute;
-        let second;
-        let millisecond;
+  function renderElapsedTime(manual_time = null) {
+    let minute;
+    let second;
+    let millisecond;
 
-        /* 経過時間を決定する */
-        // 手動で決定する場合
-        if (manual_time !== null) {
-            minute = String(manual_time / 60000).padStart(2, '0');
-            second = String((manual_time / 1000) % 60).padStart(2, '0');
-            millisecond = String(manual_time % 1000).padStart(2, '0');
-        }
-        // 自動で決定する場合
-        else {
-            const elapsed_time = new Date(Date.now() - start_time);
-            minute = String(elapsed_time.getMinutes()).padStart(2, '0');
-            second = String(elapsed_time.getSeconds()).padStart(2, '0');
-            millisecond = String(elapsed_time.getMilliseconds()).padStart(3, '0');
-        }
-
-        /* 経過時間を表示する */
-        elapsed_time_render.textContent = `経過時間: ${minute}:${second}.${millisecond}`;
+    /* 経過時間を決定する */
+    // 手動で決定する場合
+    if (manual_time !== null) {
+      minute = String(manual_time / 60000).padStart(2, "0");
+      second = String((manual_time / 1000) % 60).padStart(2, "0");
+      millisecond = String(manual_time % 1000).padStart(2, "0");
+    }
+    // 自動で決定する場合
+    else {
+      const elapsed_time = new Date(Date.now() - start_time);
+      minute = String(elapsed_time.getMinutes()).padStart(2, "0");
+      second = String(elapsed_time.getSeconds()).padStart(2, "0");
+      millisecond = String(elapsed_time.getMilliseconds()).padStart(3, "0");
     }
 
-    /* 一定時間で表示する処理 */
-    function renderByRegularTime ()
-    {
-        /*  */
-        if (status === STATUS_TEMPLATE.FINISH){
-            return;
-        }
+    /* 経過時間を表示する */
+    elapsed_time_render.textContent = `経過時間: ${minute}:${second}.${millisecond}`;
+  }
 
-        renderElapsedTime();
-        rest_word_num_render.textContent = `残り単語数: ${rest_word_num}`;
-
-        setTimeout(() => {
-            renderByRegularTime();
-        }, 10);
+  /* 一定時間で表示する処理 */
+  function renderByRegularTime() {
+    /*  */
+    if (status === STATUS_TEMPLATE.FINISH) {
+      return;
     }
 
-    oneGamePrepare();
+    renderElapsedTime();
+    rest_word_num_render.textContent = `残り単語数: ${rest_word_num}`;
 
-    document.addEventListener('keydown', e =>
-    {
-        /* 開始していない場合 */
-        if (status === STATUS_TEMPLATE.READY) {
-            /* 開始しない場合 */
-            if (e.key !== word[location]) {
-                return;
-            }
+    setTimeout(() => {
+      renderByRegularTime();
+    }, 10);
+  }
 
-            /* 開始前の処理 */
-            result.textContent = '';
-            status = STATUS_TEMPLATE.START;
-            renderByRegularTime();
-            start_time = Date.now();
-        }
+  oneGamePrepare();
 
-        /* 終了している場合 */
-        if (status === STATUS_TEMPLATE.FINISH) {
-            /* リセット処理しない場合 */
-            if (e.key !== 'Enter') {
-                return;
-            }
+  document.addEventListener("keydown", (e) => {
+    /* 開始していない場合 */
+    if (status === STATUS_TEMPLATE.READY) {
+      /* 開始しない場合 */
+      if (e.key !== word[location]) {
+        return;
+      }
 
-            /* リセット処理 */
-            oneGamePrepare();
+      /* 開始前の処理 */
+      result.textContent = "";
+      status = STATUS_TEMPLATE.START;
+      renderByRegularTime();
+      start_time = Date.now();
+    }
 
-            return;
-        }
+    /* 終了している場合 */
+    if (status === STATUS_TEMPLATE.FINISH) {
+      /* リセット処理しない場合 */
+      if (e.key !== "Enter") {
+        return;
+      }
 
-        /* ミスタッチをした場合 */
-        if (e.key !== word[location]) {
-            if (e.key === 'Shift') {
-                return;
-            }
-            /* ミスタッチをした場合の処理 */
-            if (!target.classList.contains('mistake')){
-                target.classList.add('mistake');
-            }
+      /* リセット処理 */
+      oneGamePrepare();
 
-            mistake_count++;
+      return;
+    }
 
-            mistake_render.textContent = `ミスタッチ: ${mistake_count}`;
-            return;
-        }
+    /* ミスタッチをした場合 */
+    if (e.key !== word[location]) {
+      if (e.key === "Shift") {
+        return;
+      }
+      /* ミスタッチをした場合の処理 */
+      if (!target.classList.contains("mistake")) {
+        target.classList.add("mistake");
+      }
 
-        /* 正常に入力された場合の処理 */
-        location++;
-        target.textContent = '_'.repeat(location) + word.substring(location);
-        if (target.classList.contains('mistake')){
-            target.classList.remove('mistake');
-        }
+      mistake_count++;
 
-        /* 1単語を打ち終わっていない場合 */
-        if (location !== word.length) {
-            return;
-        }
+      mistake_render.textContent = `ミスタッチ: ${mistake_count}`;
+      return;
+    }
 
-        /* 1単語を打ち終わった場合の処理 */
-        rest_word_num--;
-        setWord();
+    /* 正常に入力された場合の処理 */
+    location++;
+    target.textContent = "_".repeat(location) + word.substring(location);
+    if (target.classList.contains("mistake")) {
+      target.classList.remove("mistake");
+    }
 
-        /* 全ての単語を打ち終わっていない場合 */
-        if(rest_word_num > 0) {
-            return;
-        }
+    /* 1単語を打ち終わっていない場合 */
+    if (location !== word.length) {
+      return;
+    }
 
-        /* 全ての単語を打ち終わった場合の処理 */
-        status = STATUS_TEMPLATE.FINISH;
-        renderElapsedTime();
-        target.textContent = '';
-        rest_word_num_render.textContent = `残り単語数: ${rest_word_num}`;
-        mistake_render.textContent = `ミスタッチ: ${mistake_count}`;
-        result.textContent = '終了しました\nもう一度やるにはエンターキーを押して下さい。';
-    });
+    /* 1単語を打ち終わった場合の処理 */
+    rest_word_num--;
+    setWord();
+
+    /* 全ての単語を打ち終わっていない場合 */
+    if (rest_word_num > 0) {
+      return;
+    }
+
+    /* 全ての単語を打ち終わった場合の処理 */
+    status = STATUS_TEMPLATE.FINISH;
+    renderElapsedTime();
+    target.textContent = "";
+    rest_word_num_render.textContent = `残り単語数: ${rest_word_num}`;
+    mistake_render.textContent = `ミスタッチ: ${mistake_count}`;
+    result.textContent =
+      "終了しました\nもう一度やるにはエンターキーを押して下さい。";
+  });
 }
--- Js/updateHistory.js
+++ Js/updateHistory.js
@@ -1,98 +1,109 @@
-'use strict'
+"use strict";
 
 // 更新履歴をJavaScriptで操作
 {
-    function AddContent (day, content)
-    {
-        const update_history_contents = document.getElementById('update-history-contents');
-        const update_history_content_li = document.createElement('li');
-        update_history_content_li.classList.add('update-history-content-li');
-        const update_history_content_frame = document.createElement('dl');
-        update_history_content_frame.id =
-            `update-history-content-frame-${update_history_contents.childElementCount}`;
-        update_history_content_frame.classList.add('update-history-content-frame');
+  const addContent = (day, content) => {
+    const update_history_contents = document.getElementById(
+      "update-history-contents"
+    );
+    const update_history_content_li = document.createElement("li");
+    update_history_content_li.classList.add("update-history-content-li");
+    const update_history_content_frame = document.createElement("dl");
+    update_history_content_frame.id = `update-history-content-frame-${update_history_contents.childElementCount}`;
+    update_history_content_frame.classList.add("update-history-content-frame");
 
-        // 更新日
-        const update_history_day = document.createElement('dt');
-        update_history_day.classList.add('update-history-day');
-        update_history_day.textContent = `${day.year}.${day.month}.${day.date}`;
-        update_history_content_frame.appendChild(update_history_day);
+    // 更新日
+    const update_history_day = document.createElement("dt");
+    update_history_day.classList.add("update-history-day");
+    update_history_day.textContent = `${day.year}.${day.month}.${day.date}`;
+    update_history_content_frame.appendChild(update_history_day);
 
-        // 更新内容
-        const update_history_content = document.createElement('dd');
-        update_history_content.classList.add('update-history-content');
-        const update_history_content_pre = document.createElement('pre');
-        update_history_content_pre.textContent = `${content}`;
-        update_history_content.appendChild(update_history_content_pre);
-        update_history_content_frame.appendChild(update_history_content);
+    // 更新内容
+    const update_history_content = document.createElement("dd");
+    update_history_content.classList.add("update-history-content");
+    const update_history_content_pre = document.createElement("pre");
+    update_history_content_pre.textContent = `${content}`;
+    update_history_content.appendChild(update_history_content_pre);
+    update_history_content_frame.appendChild(update_history_content);
 
-        update_history_content_li.appendChild(update_history_content_frame);
-        if (update_history_contents.childElementCount >= 1) {
-            update_history_contents.insertBefore(
-                update_history_content_li, update_history_contents.firstChild);
-        }
-        else {
-            update_history_contents.appendChild(update_history_content_li);
-        }
+    update_history_content_li.appendChild(update_history_content_frame);
+    if (update_history_contents.childElementCount >= 1) {
+      update_history_contents.insertBefore(
+        update_history_content_li,
+        update_history_contents.firstChild
+      );
+    } else {
+      update_history_contents.appendChild(update_history_content_li);
     }
+  };
 
-    // 2020.9/4
-    AddContent(
-        {
-            year: 2020,
-            month: 9,
-            date: 4,
-        },
-        'Webページ「JPig」を公開しました。'
-    );
+  // 2020.9/4
+  addContent(
+    {
+      year: 2020,
+      month: 9,
+      date: 4,
+    },
+    "Webページ「JPig」を公開しました."
+  );
 
-    // 2020.9/20
-    AddContent(
-        {
-            year: 2020,
-            month: 9,
-            date: 20,
-        },
-        'Contentsに複数タイマー追加しました。'
-    );
+  // 2020.9/20
+  addContent(
+    {
+      year: 2020,
+      month: 9,
+      date: 20,
+    },
+    "Contentsに複数タイマー追加しました."
+  );
 
-    // 2020.9/24
-    AddContent(
-        {
-            year: 2020,
-            month: 9,
-            date: 24,
-        },
-        'デザインを変更しました。\nカレンダーと世界時計を追加しました。'
-    );
+  // 2020.9/24
+  addContent(
+    {
+      year: 2020,
+      month: 9,
+      date: 24,
+    },
+    "デザインを変更しました.\nカレンダーと世界時計を追加しました."
+  );
 
-    // 2020.10/11
-    AddContent(
-        {
-            year: 2020,
-            month: 10,
-            date: 11,
-        },
-        'Homeページを中心にデザインを変更しました。\nコードを少し整理しました。'
-    )
+  // 2020.10/11
+  addContent(
+    {
+      year: 2020,
+      month: 10,
+      date: 11,
+    },
+    "Homeページを中心にデザインを変更しました.\nコードを少し整理しました."
+  );
 
-    // 2020.10/14
-    AddContent(
-        {
-            year: 2020,
-            month: 10,
-            date: 14,
-        },
-        'デザインをシンプルにしました。\nレスポンスデザインに対応しました。'
-    );
+  // 2020.10/14
+  addContent(
+    {
+      year: 2020,
+      month: 10,
+      date: 14,
+    },
+    "デザインをシンプルにしました.\nレスポンスデザインに対応しました."
+  );
 
-    // 2020.11/2
-    AddContent(
-        {
-            year: 2020,
-            month: 11,
-            date: 2,
-        },
-        'デザインを新しくして, よりシンプルにしました。\nContentsにタイピングゲームとToDoリストを追加しました。\n※デザインを新しくしたため, レスポンスデザインに対応していません。'
-    );
+  // 2020.11/2
+  addContent(
+    {
+      year: 2020,
+      month: 11,
+      date: 2,
+    },
+    "デザインを新しくして, よりシンプルにしました.\nContentsにタイピングゲームとToDoリストを追加しました.\n※デザインを新しくしたため, レスポンスデザインに対応していません."
+  );
+
+  // 2020.12/23
+  addContent(
+    {
+      year: 2020,
+      month: 12,
+      date: 23,
+    },
+    "プロフィールを変更しました."
+  );
 }
